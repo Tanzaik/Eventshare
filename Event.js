efddef02269237043d38d85e636b505f77da393a
Tanzim Amin <37065007+Tanzaik@users.noreply.github.com>
2025-04-13 11:22:07 -0700

    Create Event.js

---
+++ Event.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  date: { type: Date, required: true },
+  location: String,
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Event", eventSchema);
